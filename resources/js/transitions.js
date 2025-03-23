5f6650dcf970c348b44c016c558e80fa6fd66661
Ricardo Henriquez <ricardo@tradeledge.com>
2025-03-23 13:30:04 +0000

    finished landing page design with placeholders for social media

--- resources/js/transitions.js
+++ resources/js/transitions.js
@@ -1,22 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     const bg1 = document.getElementById("bg1");
     const bg2 = document.getElementById("bg2");
+    const bg3 = document.getElementById("bg3");
     const credit1 = document.getElementById("credit1");
     const credit2 = document.getElementById("credit2");
-    let showingBg1 = true;
+    const credit3 = document.getElementById("credit3");
+    let showing = "bg1";
 
     setInterval(() => {
-      if (showingBg1) {
+      if (showing === "bg1") {
         bg1.classList.add("opacity-0");
         bg2.classList.remove("opacity-0");
         credit1.classList.add("opacity-0");
         credit2.classList.remove("opacity-0");
-      } else {
-        bg1.classList.remove("opacity-0");
+        showing = "bg2"
+      } else if (showing === "bg2") {
         bg2.classList.add("opacity-0");
-        credit1.classList.remove("opacity-0");
+        bg3.classList.remove("opacity-0");
         credit2.classList.add("opacity-0");
+        credit3.classList.remove("opacity-0");
+        showing = "bg3"
+      } else {
+        bg3.classList.add("opacity-0");
+        bg1.classList.remove("opacity-0");
+        credit3.classList.add("opacity-0");
+        credit1.classList.remove("opacity-0");
+        showing = "bg1";
       }
-      showingBg1 = !showingBg1;
+      
     }, 6000); // switch every 6 seconds
   });
