b21b6048948ad087dc6d30558383dad3d0352313
Ricardo Henriquez <ricardo@tradeledge.com>
2025-03-24 20:36:39 +0000

    Added favicon & 3rd background for DE & ES

--- resources/js/transitions.js
+++ resources/js/transitions.js
@@ -7,26 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const credit3 = document.getElementById("credit3");
     let showing = "bg1";
 
-    setInterval(() => {
-      if (showing === "bg1") {
-        bg1.classList.add("opacity-0");
-        bg2.classList.remove("opacity-0");
-        credit1.classList.add("opacity-0");
-        credit2.classList.remove("opacity-0");
-        showing = "bg2"
-      } else if (showing === "bg2") {
-        bg2.classList.add("opacity-0");
-        bg3.classList.remove("opacity-0");
-        credit2.classList.add("opacity-0");
-        credit3.classList.remove("opacity-0");
-        showing = "bg3"
-      } else {
-        bg3.classList.add("opacity-0");
-        bg1.classList.remove("opacity-0");
-        credit3.classList.add("opacity-0");
-        credit1.classList.remove("opacity-0");
-        showing = "bg1";
-      }
-      
-    }, 6000); // switch every 6 seconds
-  });
+  setInterval(() => {
+    if (showing === "bg1") {
+      bg1.classList.add("opacity-0");
+      bg2.classList.remove("opacity-0");
+      credit1.classList.add("opacity-0");
+      credit2.classList.remove("opacity-0");
+      showing = "bg2"
+    } else if (showing === "bg2") {
+      bg2.classList.add("opacity-0");
+      bg3.classList.remove("opacity-0");
+      credit2.classList.add("opacity-0");
+      credit3.classList.remove("opacity-0");
+      showing = "bg3"
+    } else {
+      bg3.classList.add("opacity-0");
+      bg1.classList.remove("opacity-0");
+      credit3.classList.add("opacity-0");
+      credit1.classList.remove("opacity-0");
+      showing = "bg1";
+    }
+  }, 6000); // switch every 6 seconds
+});
